66cc5e86f2c0bfc6eb27f4dd3b59a4edd616042d
davide gallinella <d.gallinella@reply.it>
2021-05-10 18:35:32 +0200

    fix

---
+++ employeemanager/src/main/java/com/demo/employeemanager/model/Employee.java
@@ -0,0 +1,15 @@
+package com.demo.employeemanager.model;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable{
+	
+	private Long id;
+	private String email;
+	private String name;
+	private String jobTitle;
+	private String phone;
+	private String imageUrl;
+	private String employeeCode;
+	
+}
